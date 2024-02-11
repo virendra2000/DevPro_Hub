712e7fd29a5c08915d6c0222f076d78ff4cdcfcb
virendra2000 <vkalwar132000@gmail.com>
2024-02-11 14:32:53 +0530

    Update

---
+++ components/project.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image"
+import { PROJECT_DETAIL } from "@/constants"
+import Link from "next/link"
+const Project = () => {
+    return (
+        <>
+            <section className="p-5 h-auto bg-slate-100 dark:bg-slate-800 flex flex-col items-center justify-center w-full">
+                <h2 className="head3 p-2 text-black dark:text-white text-4xl font-bold">My <span className="text-blue-600 dark:text-yellow-300">Projects</span></h2>
+
+                <div className="skill-container mt-5 w-full flex flex-row flex-wrap gap-4 items-center justify-center">
+                {PROJECT_DETAIL.map((link) => (
+                    <div key={link.key} className="project-card hover:text-opacity-100 cursor-pointer">
+                        <Image src={link.imgurl}
+                        alt={link.altdesc}
+                        width={900}
+                        height={900}
+                        className="image"/>
+
+                        <div className="text-overlay flex flex-col items-center justify-center">
+                            <h2 className="head-txt text-2xl font-bold">{link.projecttitle}</h2>
+                            <p className="p-2 head-txt text-md font-bold">
+                                {link.projectdesc}
+                            </p>
+                            <Link href={link.href}>
+                                <div className="p-2 rounded-full cursor-pointer bg-black hover:bg-orange-500 text-white">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m13 3 3.293 3.293-7 7 1.414 1.414 7-7L21 11V3z"/><path fill="currentColor" d="M19 19H5V5h7l-2-2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2h14c1.103 0 2-.897 2-2v-5l-2-2v7z"/></svg>
+                                </div>
+                            </Link>
+                        </div>
+                    </div>
+                ))}
+                </div>
+            </section>
+        </>
+    )
+}
+export default Project
